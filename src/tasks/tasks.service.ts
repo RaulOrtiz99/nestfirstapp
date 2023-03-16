ce37825cbbc0e982408a987269986da3cf16c148
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2023-03-16 00:32:20 -0400

    devolviendo todas las tareras, y usando DTOS para la transferencias de archivos, y el tipico createTasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import {v4} from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'Task 1 description',
+      status: TaskStatus.PENDING,
+    },
+  ];
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTasks(title: string, description: string) {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push();
+
+    return task;
+  }
+  updateTasks() {}
+  deleteTasks() {}
+}
